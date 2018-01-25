f8b9bef3e9023d9cba9046ae759986b74f361569
mvfontes <marcelavfontes@gmail.com>
2018-01-24 22:01:12 -0300

    [Playground] Plays with class inheritance

--- src/playground/es6-classes-1.js
+++ src/playground/es6-classes-1.js
@@ -8,7 +8,7 @@ class Person {
     }
 
     getGreeting(){
-        return `Hi! I am ${this.name}`;
+        return `Hi! I am ${this.name}.`;
     }
 
     getDescription(){
@@ -16,8 +16,56 @@ class Person {
     }
 }
 
-const me = new Person('Kerveros', 26);
+class Student extends Person {
+    constructor(name, age, major){
+        super(name, age);
+        this.major = major;
+    }
+
+    hasMajor(){
+        return !!this.major;
+    }
+
+    getDescription(){
+        let description = super.getDescription();
+
+        if (this.hasMajor()){
+            description += ` Their major is ${this.major}`;
+        }
+
+        return description;
+    }
+}
+
+class Traveler extends Person {
+    constructor(name, age, homeLocation){
+        super(name, age);
+        this.homeLocation = homeLocation;
+    }
+
+    hasHomeLocation(){
+        return !!this.homeLocation;
+    }
+
+    getGreeting(){
+        let greeting = super.getGreeting();
+
+        if (this.hasHomeLocation()){
+            greeting += ` I'm visiting from ${this.homeLocation}.`
+        }
+
+        return greeting;
+    }
+}
+
+const me = new Student('Kerveros', 26, 'Computer Science');
 console.log(me.getDescription());
 
-const other = new Person();
+const other = new Student();
 console.log(other.getDescription());
+
+const me2 = new Traveler('Kerveros', 26, 'Natal');
+console.log(me2.getGreeting());
+
+const other2 = new Traveler();
+console.log(other2.getGreeting());
