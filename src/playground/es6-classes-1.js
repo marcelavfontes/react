6c5776b9449853c6e8d2496fa442220fff491ea2
mvfontes <marcelavfontes@gmail.com>
2018-01-24 21:31:00 -0300

    [Playground] Creates class in javascript

---
+++ src/playground/es6-classes-1.js
@@ -0,0 +1,23 @@
+
+class Person {
+    //only method that gets called implicitly   
+    //setting default value
+    constructor(name = 'Anonymous', age = 0){
+        this.name = name;
+        this.age = age;
+    }
+
+    getGreeting(){
+        return `Hi! I am ${this.name}`;
+    }
+
+    getDescription(){
+        return `${this.name} is ${this.age} year(s) old.`
+    }
+}
+
+const me = new Person('Kerveros', 26);
+console.log(me.getDescription());
+
+const other = new Person();
+console.log(other.getDescription());
