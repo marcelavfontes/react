edcfcd4bcb8ff415bffddafc44440395fc34aed0
mvfontes <marcelavfontes@gmail.com>
2018-01-28 20:49:14 -0300

    First try at React components

--- src/app.js
+++ src/app.js
@@ -1,61 +1,57 @@
-console.log('App.js is running!');
-
-//JSX - JavaScript XML -> extension of JS
-
-const app = {
-    title: 'Indecision App',
-    subtitle: 'Put your life in the hands of a computer',
-    options: []
-}
-
-const onFormSubmit = (e) => {
-    e.preventDefault();
-
-    const option = e.target.elements.option.value;
-
-    if(option){
-        app.options.push(option);
-        e.target.elements.option.value = '';
-        renderApp();
+    class Header extends React.Component {
+        //with React components we must define render
+        render(){
+            return (
+                <div>
+                    <h1>Indecision</h1>
+                    <h2>Put your life in the hands of a computer.</h2>
+                </div>
+            );
+        }
     }
-};
 
-const onRemoveAll = () => {
-    app.options = [];
-    renderApp();
-};
+    class Action extends React.Component {
+        render(){
+            return(
+                <div>
+                    <button>What should I do?</button>
+                </div>
+            );
+        }
+    }
 
-const onMakeDecision = () => {
-    const randomNum = Math.floor(Math.random() * app.options.length);
-    const option = app.options[randomNum];
-    alert(option);
-}
+    class Options extends React.Component {
+        render(){
+            return(
+                <div> 
+                    <ol>
+                        <li>Options component here</li>
+                    </ol>
+                </div>
+            );
+        }
+    }
 
-const appRoot = document.getElementById('app');
+    class AddOption extends React.Component {
+        render(){
+            return(
+                <div>
+                <form>
+                    <input type="text"/>
+                    <button>Add options</button>
+                </form>
+                </div>
+            );
+        }
+    }
 
-const renderApp = () => {
-    const template = (
-        //undefined, null and booleans are ignored by JSX
+    const jsx = (
         <div>
-            <h1>{app.title}</h1> 
-            {(app.subtitle && <p>{app.subtitle}</p>)}
-            <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
-            <button disabled={app.options.length === 0} onClick={onMakeDecision}>What should I do?</button>
-            <button onClick={onRemoveAll}>Remove All</button>
-            <ol>
-                {
-                    app.options.map((option) => <li key={option}>{option}</li>)
-                }
-            </ol>
-            <form onSubmit={onFormSubmit}>
-                <input type="text" name="option"/>
-                <button>Add Option</button>
-            </form>
+            <Header />
+            <Action />
+            <Options />
+            <AddOption />
         </div>
     );
-    //<h1>{user.name}</h1> -> The {} is a Javascript expression. We cannot put statements inside (like `if` ) 
-    ReactDOM.render(template, appRoot);
- };
 
-renderApp();
-
+    ReactDOM.render(jsx, document.getElementById('app'));
